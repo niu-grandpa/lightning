a6e60ba93e2c5e60a1056288adcd8f448917405c
niu-grandpa <2864103063@qq.com>
2022-11-29 17:03:13 +0800

    init

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@ import { createApp } from 'vue';
 import { Button, Toast } from '@nutui/nutui-taro';
 
 import './app.less';
-import { usePlugins } from './hooks';
 
 const App = createApp({
   onShow(options) {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 });
 
-usePlugins(App, [Button, Toast]);
+App.use(Button).use(Toast);
 
 export default App;
